54d7abc6b467b3d95ccf375ea1d02f12b3ae72bb
foxt451 <svpetrenko123@gmail.com>
2022-12-07 15:31:46 +0200

    initial scraper boilerplate

---
+++ TODO.md
@@ -0,0 +1,1 @@
+1. Remove got-scraping if eventually you have to switch to playwright or it is integrated
---
+++ src/PoC.ts
@@ -0,0 +1,38 @@
+import { gotScraping } from "got-scraping";
+
+const testHeaders = `Content-Type: application/json
+Quora-Formkey: e60a6daaf5513fff206677cadb0e9683
+Cookie: m-b_strict=OMbPotJA0pFIWjyZJ97h8g==;`;
+
+const obj = testHeaders.split("\n").reduce((prev, cur) => {
+    const [key, value] = cur.split(": ");
+    return { ...prev, [key]: value };
+}, {});
+
+console.log(obj);
+
+const result = await gotScraping.post(
+    "https://www.quora.com/graphql/gql_para_POST?q=SearchResultsListQuery",
+    {
+        body: JSON.stringify({
+            queryName: "SearchResultsListQuery",
+            variables: {
+                query: "how to walk",
+                disableSpellCheck: null,
+                resultType: "all_types",
+                author: null,
+                time: "all_times",
+                first: 10,
+                after: "19",
+                tribeId: null,
+            },
+            extensions: {
+                hash: "9dce4809922a976f52b021ed9bbfb4d49bff450675bad8c8913518b0e100d3c4",
+            },
+        }),
+        headers: obj,
+    }
+);
+console.log(result.statusCode, result.statusMessage);
+console.log(result.body);
+console.log(result.rawHeaders);
